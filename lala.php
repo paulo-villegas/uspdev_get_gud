780828c5c9eda68586763eed27bba1b1123d361a
paulo-villegas <paulo.villegas@poli.usp.br>
2018-08-20 16:57:16 -0300

    Inline bar argument

--- lala.php
+++ lala.php
@@ -2,6 +2,10 @@
 
 bar_dump("lala");
 
-function bar_dump($var){
-    var_dump($var);
+function bar_dump($var, $inline = false){
+    if($inline){
+        printf($var, true);
+    }else{
+        var_dump($var);
+    }
 }
